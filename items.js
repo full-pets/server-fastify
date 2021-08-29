4b10aa296d6cc9c8affba0730262fde91d08f08d
Andrei Prud <andrei.prud@voxonics.com>
2021-08-29 21:29:42 +0300

    crash-course

---
+++ items.js
@@ -0,0 +1,8 @@
+let items = [
+    {id: '1', name: 'Items One'},
+    {id: '2', name: 'Items Two'},
+    {id: '3', name: 'Items Three'},
+    {id: '4', name: 'Items Four'},
+]
+
+module.exports = items
